93fd431c2f6235acece8c8963c99e3d900fb445c
MarinescuAndrei2003 <165507300+MarinescuAndrei2003@users.noreply.github.com>
2024-05-30 21:47:09 +0300

    Add files via upload

--- Homework/main.py
+++ Homework/main.py
@@ -1,5 +1,7 @@
 import heapq
 import itertools
+from timeit import default_timer as timer
+from datetime import timedelta
 from dfs import tsp_dfs_wrapper
 from least_cost_search import tsp_least_cost
 from a_star import tsp_a_star
@@ -16,21 +18,46 @@ from random_generator import generate_tsp_graph
 # Bucuresti[0] -> Timisoara[2] -> Cluj[1] -> Iasi[3] -> Constanta[4] -> Bucuresti[0]
 
 graph = [[0, 450, 530, 410, 225],
-        [450, 0, 320, 380, 600],
-        [530, 320, 0, 500, 550],
-        [410, 380, 500, 0, 370],
-        [225, 600, 550, 370, 0]]
+         [450, 0, 320, 380, 600],
+         [530, 320, 0, 500, 550],
+         [410, 380, 500, 0, 370],
+         [225, 600, 550, 370, 0]]
 
+start = timer()
 print(tsp_dfs_wrapper(graph))
+end = timer()
+print("DFS: ", timedelta(seconds=end-start))
+
+start = timer()
 print(tsp_least_cost(graph))
+end = timer()
+print("Least_cost_search: ", timedelta(seconds=end-start))
+
+start = timer()
 print(tsp_a_star(graph))
+end = timer()
+print("A* search:  ", timedelta(seconds=end-start))
 
 print("-------------------------------------------------------------------------")
 
 graph1 = generate_tsp_graph(10)
 
-print("graph1:", graph1)
+print("graph1:\n", graph1)
 
+
+start = timer()
 print(tsp_dfs_wrapper(graph1))
+end = timer()
+print("DFS: ", timedelta(seconds=end-start))
+
+
+start = timer()
 print(tsp_least_cost(graph1))
+end = timer()
+print("Least_cost_search: ", timedelta(seconds=end-start))
+
+
+start = timer()
 print(tsp_a_star(graph1))
+end = timer()
+print("A* search:  ", timedelta(seconds=end-start))
